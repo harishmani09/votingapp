df5904b3f9573f9373b25c3ef924c344ddc683d8
harishmani09 <harishmani@gmail.com>
2022-10-28 17:10:51 +0530

    episode 18,tests for votes dones

--- app/Http/Livewire/IdeaShow.php
+++ app/Http/Livewire/IdeaShow.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Idea;
 use Livewire\Component;
 
 class IdeaShow extends Component
@@ -10,7 +11,11 @@ class IdeaShow extends Component
     public $idea;
     public $votesCount;
 
-
+    public function mount(Idea $idea, $votesCount)
+    {
+        $this->idea = $idea;
+        $this->votesCount = $votesCount;
+    }
 
 
     public function render()
